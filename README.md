426ca3a77787812a0a09631a5c1014b7db83f220
Zelite <jcafernandes@gmail.com>
2016-09-07 17:25:08 +0000

    Added math.js for dealing with big numbers and formatting them. Added Keyboard interaction.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # JavaScript-Calculator
 
-Build a JavaScript Calculator
+Build a JavaScript Calculator.
+
+Using separation of concerns as learned with the 
+[Javascript Design Patterns](https://classroom.udacity.com/courses/ud989/) course.
--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,10 @@
 /*global $*/
+math.config({
+  number: 'BigNumber', // Default type of number:
+                       // 'number' (default), 'BigNumber', or 'Fraction'
+  precision: 64        // Number of significant digits for BigNumbers
+});
+
 var operators = "+-&divide;&times;÷×";
 var operatorConversion = {
     "+": "+",
@@ -6,7 +12,9 @@ var operatorConversion = {
     "&divide;": "/",
     "&times;":"*",
     "÷": "/",
-    "×": "*"
+    "×": "*",
+    "*": "×",
+    "/": "÷"
 };
 
 function isOperator(char){
@@ -29,7 +37,7 @@ var model = {
             chain = chain.replace( /×/g, "*");
             if(isOperator(model.currentChain.slice(-1)))
                 chain = chain.slice(0, -1);
-            this.currentResult = eval(chain);
+            this.currentResult = math.eval(chain);
         }
     }
 };
@@ -37,10 +45,15 @@ var model = {
 // Using octopus as an alias for controller - inspired by this course:
 // https://www.udacity.com/course/javascript-design-patterns--ud989
 var octopus = {
-    getCurrentResult: function() {return model.currentResult},
+    getCurrentResult: function() {
+        var result = model.currentResult;
+        if(result === ""){
+            return "";
+        }else{
+            return math.format(result, 10);
+        }},
     getCurrentChain: function() {return model.currentChain},
     pressedDigit: function(){
-        debugger;
         var pressedNumber = $(this).text();
        if(model.currentNumber === "0"){ //if theres a zero, we dont want repeated zeros
                 model.currentChain = model.currentChain.slice(0, -1)+pressedNumber;
@@ -52,6 +65,7 @@ var octopus = {
         view.render();
     },
     pressedDecimal: function(){
+        
         if(model.currentNumber.indexOf(".") === -1){
             model.currentNumber += ".";
             model.currentChain += ".";
@@ -96,7 +110,23 @@ var octopus = {
             model.currentNumber = result.toString();
         }
         view.render();
-    }
+    },
+    keyboard: function(event){
+
+        if ( event.which == 13 ) {//if enter, make the result appear
+          event.preventDefault();
+          $("button.equal").click();
+        }
+        else if("123456789".indexOf(event.key) != -1) {//if one of the numbers
+            $("button.number:contains("+event.key+")").click();
+        }
+        else if(event.keyCode == 44){
+            $("button.decimal").click();
+        }
+        else if("+-*/".indexOf(event.key) != -1){
+            var op = operatorConversion[event.key];
+            $("button.operator:contains("+op+")").click();
+        }}
 };
 
 var view = {
@@ -107,6 +137,7 @@ var view = {
         $("button.clearAll").click(octopus.clearAll);
         $("button.correctEntry").click(octopus.correctEntry);
         $("button.equal").click(octopus.equal);
+        $("body").keypress(octopus.keyboard);
         this.render();
         
     },
